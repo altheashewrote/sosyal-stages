681147c72c5c945d8c8d80fe1e39b87cc3359b44
althea rosales <altheashewrote@altheas-DAW-3.local>
2026-02-25 11:15:41 -0800

    initial commit

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,4 @@
+'use client'
+import Button from "../ui/Button"
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
--- frontend/src/components/layout/MenuBar.tsx
+++ frontend/src/components/layout/MenuBar.tsx
@@ -1,11 +1,14 @@
 'use client'
 import { useEffect, useState } from 'react';
+import Button from '../ui/Button';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 export default function MenuBar() {
     return (
-        <nav className="bg-transparent flex flex-row justify-between">
-            
+        <nav>
+            <div className="bg-transparent flex flex-row justify-between">
+
+            </div>
         </nav>
     )
 }
