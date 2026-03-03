ff250609347859b3470ae6b0ce8406d9af4fff85
althea rosales <altheashewrote@wifi-10-43-223-177.wifi.berkeley.edu>
2026-03-03 15:11:48 -0800

    did sum stuff

---
+++ frontend/src/components/home/HeroSection.tsx
@@ -0,0 +1,11 @@
+import Button from "../ui/Button";
+import { useEffect, useState } from 'react';
+
+export default function HeroSection() {
+    return (
+        <div className='p-10 justify-evenly flex flex-col bg-transparent'>
+            <h3 className='text-lg font-horizon'>SETTING THE STAGE FOR SKYROCKETING ARTISTS.</h3>
+            
+        </div>
+    )
+}
